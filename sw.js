132b732f99c5d5ef4af5444e41903dd16354851a
Chris Vincent <cvincent253@gmail.com>
2026-06-16 06:42:20 -0500

    v1.3.1 Fill primaryUse + sideEffects for all 110 entries, fix 5 reconstitution concentrations

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 // Peptide Scheduler Service Worker
-// Version 1.3.0
+// Version 1.3.1
 
-const CACHE_NAME = 'peptide-scheduler-v3';
+const CACHE_NAME = 'peptide-scheduler-v4';
 const ASSETS = [
   './',
   './index.html',
